0fff38ab1605f441431b88ddb235d647555038ea
LEECHANHYUNG <lsj721538@gmail.com>
2023-06-20 15:33:16 +0900

    feat(application): 기본 세팅 및 헤더 추가

---
+++ src/client/components/MainHeader/MainHeader.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './MainHeader.module.scss';
+const MainHeader = () => {
+  return <header className={styles.MainHeader}>Header</header>;
+};
+
+export default MainHeader;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
